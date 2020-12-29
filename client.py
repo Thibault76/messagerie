0d9ddabe9bc5cca9227b98152d5b3da59a10e8b7
Thibault76 <54622891+Thibault76@users.noreply.github.com>
2020-12-29 17:04:42 +0100

    update client

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
 
+"""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 1111))
+s.connect(("192.168.1.2", 1111))
 
 print("Le nom du fichier que vous voulez récupérer:")
 file_name = input(">> ")
@@ -11,3 +12,20 @@ r = s.recv(9999999)
 with open(file_name,'wb') as _file:
     _file.write(r)
 print("Le fichier a été correctement copié dans : %s." % file_name)
+"""
+
+
+class Client:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.s.connect(("192.168.1.2", 1111))
+
+    def send_message(self, msg):
+        self.s.send(msg.encode())
+
+    def receive_msg(self):
+        r = self.s.recv(9999999)
+        return r
+
